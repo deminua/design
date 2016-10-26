aabc6ae3f7ee5d44b6ad996ce5d034d321a87d2d
Dem <dstaranenko@gmail.com>
2016-10-26 15:13:09 +0300

    oauth api google update

--- app/Http/Controllers/ApiClientController.php
+++ app/Http/Controllers/ApiClientController.php
@@ -105,43 +105,21 @@ public function oauth2callback(Request $request)
 {
     // get data from request
     $code = $request->get('code');
-
-    // get google service
     $googleService = \OAuth::consumer('Google');
 
-    // check if code is valid
-
-    // if code is provided get user data and sign in
     if (!is_null($code))
     {
-        // This was a callback request from google, get the token
-
         $token = $googleService->requestAccessToken($code);
 
-        echo $token->access_token;
-        echo $token->token_expires;
-        return dd($token);
+        $this->token = $token->getaccessToken();
+        $this->token_expires = $token->getendOfLife();
 
-        $this->token = $token->access_token;
-        $this->token_expires = $token->token_expires;
-
-        session()->put('access_token', $token->access_token);
-        session()->put('access_token_expires', $token->token_expires);
+        session()->put('access_token', $this->token);
+        session()->put('access_token_expires', $this->token_expires);
         session()->save();
 
-        #$this->setToken($token->accessToken, $token->endOfLife);
-        
-        return $token;
+        return redirect '/';
 
-        // Send a request with it
-        // $result = json_decode($googleService->request('https://www.googleapis.com/oauth2/v1/userinfo'), true);
-
-        // $message = 'Your unique Google user id is: ' . $result['id'] . ' and your name is ' . $result['name'];
-        // echo $message. "<br/>";
-
-        // //Var_dump
-        // //display whole array.
-        // dd($result);
     }
     // if not ask for permission first
     else
