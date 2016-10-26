635d9c4707c088a462d6f4c23f584004729e7041
Dem <dstaranenko@gmail.com>
2016-10-26 12:16:10 +0300

    google api

--- app/Http/Controllers/ApiClientController.php
+++ app/Http/Controllers/ApiClientController.php
@@ -95,7 +95,7 @@ public function oauth2callback(Request $request)
             ],
         ]);
 
-        $response = $client->post($this->url.'?'.http_build_query($action))->getBody()->getContents();
+        $response = $client->post($this->url.'?'.urldecode(http_build_query($action)))->getBody()->getContents();
         return $response;
     }
 
