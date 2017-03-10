e1ec9f0e8e0a860054857834983bf2d54433170f
Dem <dstaranenko@gmail.com>
2017-03-10 15:48:25 +0200

    update 3

--- app/Project.php
+++ app/Project.php
@@ -59,23 +59,25 @@ class Project extends Model
     ];
 
     public function days()
-    {   
+    {       
+            if($this->term_at) {
             $term = Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $this->term_at);
             $term = Carbon\Carbon::parse($term);
             $start = Carbon\Carbon::now();
             return $start->diffInDays($term, false);
+            }
     }
 
     public function warning_sign()
     {   
-        if(empty($this->finish_at)) {
+        if(empty($this->finish_at) and $this->days()) {
             if($this->days() < 0) { return 'glyphicon glyphicon-warning-sign'; }
         }
     }
 
     public function warning()
     {       
-        if(empty($this->finish_at)) {
+        if(empty($this->finish_at) and $this->days()) {
                 $days = $this->days();
                 if($days <= 0) { return 'danger text-danger'; }
                 if($days <= '2') { return 'danger'; }
